ee697e57d0d47b89497c5e21cfa34331ad35a803
HansJack <2310210201@tiangong.edu.cn>
2026-03-06 16:45:04 +0800

    feat(comment): 添加作者标识徽章显示功能
    在评论系统中为文章作者添加了特殊的标识徽章，当用户发表评论时，
    如果是作者本人，会在评论者名称旁边显示一个签名图标作为作者认证标识。
    
    样式方面添加了完整的CSS规则，包括徽章的基础样式、SVG图标设置、
    悬停提示效果以及深色主题的适配，确保在不同主题下都能正常显示。

--- functions.php
+++ functions.php
@@ -5012,6 +5012,11 @@ function threadedComments($comments, $singleCommentOptions): void
                 <cite class="fn" itemprop="name"><?php $singleCommentOptions->beforeAuthor();
                     $comments->author(false);
                     $singleCommentOptions->afterAuthor(); ?></cite>
+                <?php if (strpos($commentClass, 'comment-by-author') !== false): ?>
+                    <span class="comment-author-badge" role="img" aria-label="<?php _e('笔者'); ?>" title="<?php _e('笔者'); ?>" data-comment-author-tip="<?php _e('笔者'); ?>">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-signature-icon lucide-signature" aria-hidden="true"><path d="m21 17-2.156-1.868A.5.5 0 0 0 18 15.5v.5a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1c0-2.545-3.991-3.97-8.5-4a1 1 0 0 0 0 5c4.153 0 4.745-11.295 5.708-13.5a2.5 2.5 0 1 1 3.31 3.284"/><path d="M3 21h18"/></svg>
+                    </span>
+                <?php endif; ?>
                 <?php if ($commentUrl !== ''): ?>
                     <a
                         class="comment-author-home"
